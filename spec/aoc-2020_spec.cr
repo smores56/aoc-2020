30a6ad89250902b275bb2f83562828495db7dac5
Sam Mohr <sam@mohr.codes>
2020-12-01 14:49:45 -0500

    remove tests

--- spec/aoc-2020_spec.cr
+++
@@ -1,9 +0,0 @@
-require "./spec_helper"
-
-describe Aoc do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
-end
--- spec/spec_helper.cr
+++
@@ -1,2 +0,0 @@
-require "spec"
-require "../src/aoc-2020"
