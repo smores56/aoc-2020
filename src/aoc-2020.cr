27ca37e23c80ca3e95933ef440606d1333a605af
Sam Mohr <sam@mohr.codes>
2020-12-02 01:01:08 -0500

    solve problems for day 2

--- src/aoc-2020.cr
+++ src/aoc-2020.cr
@@ -4,16 +4,16 @@ require "file_utils"
 require "./day/*"
 
 module Aoc
-  DAYS    = {
-    "1" => Day1,
-    "2" => Day2,
-    "3" => Day3,
-    "4" => Day4,
-    "5" => Day5,
-    "6" => Day6,
-    "7" => Day7,
-    "8" => Day8,
-    "9" => Day9,
+  DAYS = {
+    "1"  => Day1,
+    "2"  => Day2,
+    "3"  => Day3,
+    "4"  => Day4,
+    "5"  => Day5,
+    "6"  => Day6,
+    "7"  => Day7,
+    "8"  => Day8,
+    "9"  => Day9,
     "10" => Day10,
     "11" => Day11,
     "12" => Day12,
--- src/day/2.cr
+++ src/day/2.cr
@@ -2,12 +2,48 @@ require "./day"
 
 module Aoc
   class Day2 < Day
+    class Constraint
+      def initialize(@first : Int32, @second : Int32, @character : Char)
+      end
+
+      def self.parse(str : String)
+        if data = str.match /(\d+)-(\d+) ([a-z])/
+          Constraint.new data[1].to_i, data[2].to_i, data[3].char_at(0)
+        else
+          nil
+        end
+      end
+
+      def self.parse_with_password(str : String)
+        split = str.split ": "
+        if constraint = Constraint.parse split[0]
+          password = split[1]? || ""
+          {constraint, password}
+        else
+          nil
+        end
+      end
+    end
+
     def first(input)
-      input.size # TODO
+      input
+        .map { |i| Constraint.parse_with_password i }
+        .compact
+        .count do |constraint, password|
+          count = password.chars.count { |c| c == constraint.@character }
+          count >= constraint.@first && count <= constraint.@second
+        end
     end
 
     def second(input)
-      input.size # TODO
+      input
+        .map { |i| Constraint.parse_with_password i }
+        .compact
+        .count do |constraint, password|
+          first = password[constraint.@first - 1]? == constraint.@character
+          second = password[constraint.@second - 1]? == constraint.@character
+          first != second
+        end
     end
   end
 end
