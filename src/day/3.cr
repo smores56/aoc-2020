07acaf09c18c51f14523313b9a445037b2fe4433
Sam Mohr <sam@mohr.codes>
2020-12-03 17:54:57 -0500

    day 3 solutions

--- src/day/3.cr
+++ src/day/3.cr
@@ -1,13 +1,34 @@
 require "./day"
+require "big"
 
 module Aoc
   class Day3 < Day
+    def trees_on_path(map_rows, down, over)
+      width = map_rows[0].size
+      count, x, y = 0, 0, 0
+
+      while true
+        x += over
+        y += down
+
+        break if y >= map_rows.size
+
+        count += 1 if map_rows[y][x % width] == '#'
+      end
+
+      count
+    end
+
     def first(input)
-      input.size # TODO
+      trees_on_path(input, 1, 3)
     end
 
     def second(input)
-      input.size # TODO
+      paths = [{1, 1}, {1, 3}, {1, 5}, {1, 7}, {2, 1}]
+
+      paths
+        .map { |down, over| BigInt.new trees_on_path(input, down, over) }
+        .reduce 1 { |prod, i| prod * i }
     end
   end
 end
