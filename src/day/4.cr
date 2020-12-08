01fa663f5314745485d0d198054a8fc0ed966ea2
Sam Mohr <sam@mohr.codes>
2020-12-07 22:50:02 -0500

    finish day 4

--- src/day/4.cr
+++ src/day/4.cr
@@ -56,33 +56,33 @@ module Aoc
       end
 
       def is_valid?
-        return false unless has_required_fields?
+        constraints = [{@byr, 1920..2002}, {@iyr, 2010..2020}, {@eyr, 2020..2030}]
+        constraints.each do |value, valid_range|
+          if md = value.try &.match /^\d{4}$/
+            return false unless valid_range.covers? md[0].to_i
+          else
+            return false
+          end
+        end
 
-        return false unless (@byr || "").match(/\d{4}/) && (1920..2002).includes? (@byr || 0).to_i
-        return false unless (@iyr || "").match(/\d{4}/) && (2010..2020).includes? (@iyr || 0).to_i
-        return false unless (@eyr || "").match(/\d{4}/) && (2020..2030).includes? (@eyr || 0).to_i
-
-        if md = (@hgt || "").match /(\d+)cm/
-          return false unless (150..193).includes? md[1].to_i
-        elsif md = (@hgt || "").match /(\d+)in/
-          return false unless (59..76).includes? md[1].to_i
+        if md = @hgt.try &.match /^(\d+)cm$/
+          cm = md[1].to_i
+          return false unless (150..193).covers? cm
+        elsif md = @hgt.try &.match /^(\d+)in$/
+          inches = md[1].to_i
+          return false unless (59..76).covers? inches
         else
           return false
         end
 
-        return false unless (@hcl || "").match /\#[0-9a-f]{6}/
-        return false unless ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes? @ecl
-        return false unless (@pid || "").match /\d{9}/
+        return false unless @hcl.try &.match /^#[0-9a-f]{6}$/
+
+        eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        return false unless eye_colors.includes? (@ecl || "")
+
+        return false unless @pid.try &.match /^\d{9}$/
 
         true
-      end
-
-      def inspect(io)
-        [{@byr, "byr"}, {@iyr, "iyr"}, {@eyr, "eyr"}, {@hgt, "hgt"},
-         {@hcl, "hcl"}, {@ecl, "ecl"}, {@pid, "pid"}, {@cid, "cid"},
-        ].each do |val, name|
-          io << name << ": " << (val || "nil") << ", "
-        end
       end
     end
   end
